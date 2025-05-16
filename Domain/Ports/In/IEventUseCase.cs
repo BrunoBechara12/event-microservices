4c7b035e2735300c592351e7391838b7db3dbf41
Bruno Bechara <Brunosbechara@gmail.com>
2025-05-16 09:59:56 -0300

    feat(event):  add ports

--- Domain/Ports/In/IEventUseCase.cs
+++ Domain/Ports/In/IEventUseCase.cs
@@ -1,7 +1,17 @@
 ﻿using Domain.Entities;
+using t;
 
 namespace Domain.Ports.In;
 public interface IEventUseCase
 {
-    Task<IEnumerable<Event>> GetAllAsync();
+    Task<Result<IEnumerable<Event>>> GetAll();
+    Task<Result<Event>> GetById(int id);
+    Task<Result<IEnumerable<Event>>> GetByUserId(int userId);
+    Task<Result<Event>> Create(Event createEvent);
+    Task Update(Event updateEvent);
+    Task Delete(int id);
+    Task<Result<IEnumerable<Event>>> GetCollaborators(int userId);
+    Task AddCollaborator(int eventId, int userId);
+    Task UpdateCollaboratorRole(int userId);
+    Task RemoveCollaborator(int eventId, int userId);
 }
--- Domain/Ports/Output/IEventRepository.cs
+++ Domain/Ports/Output/IEventRepository.cs
@@ -1,7 +1,17 @@
 ﻿using Domain.Entities;
+using static Domain.Entities.Collaborator;
 
 namespace Domain.Ports.Output;
 public interface IEventRepository
 {
     Task<IEnumerable<Event>> GetAll();
+    Task<Event> GetById(int id);
+    Task<IEnumerable<Event>> GetByUserId(int userId);
+    Task<Event> Create(Event createEvent);
+    Task Update(Event updateEvent);
+    Task Delete(int id);
+    Task<IEnumerable<Collaborator>> GetCollaborators(int eventId);
+    Task AddCollaborator(int eventId, int userId, CollaboratorRole role);
+    Task UpdateCollaboratorRole(int eventId, int userId, CollaboratorRole role);
+    Task RemoveCollaborator(int eventId, int userId);
 }
