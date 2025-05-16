3d92eec63a65a0555071badcae37f0e6d2ab7381
Bruno Bechara <Brunosbechara@gmail.com>
2025-05-16 10:05:46 -0300

    feat: add Result<T> class for standardized return

---
+++ t/Result.cs
@@ -0,0 +1,26 @@
+﻿namespace t;
+
+public class Result<T>
+{
+    public bool RequestSuccess { get; }
+    public T? Value { get; }
+    public string Message { get; }
+
+    private Result(bool requestSuccess, T? value, string message)
+    {
+        RequestSuccess = requestSuccess;
+        Value = value;
+        Message = message;
+    }
+
+    public static Result<T> Success(T? value, string message)
+    {
+        return new Result<T>(true, value, message);
+    }
+
+    public static Result<T> Failure(string message)
+    {
+        return new Result<T>(false, default, message);
+    }
+
+}
