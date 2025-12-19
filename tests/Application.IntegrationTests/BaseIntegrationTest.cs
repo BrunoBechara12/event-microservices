d0a7061d923d9c17fd8d58faf2e17811213303cd
Bruno Bechara <Brunosbechara@gmail.com>
2025-12-19 19:27:50 -0300

    feat(event): add integration test setup

---
+++ tests/Application.IntegrationTests/BaseIntegrationTest.cs
@@ -0,0 +1,25 @@
+﻿using Adapters.Secondary.Data.Context;
+using Application.Ports.In;
+using Domain.Ports.Output;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.IntegrationTests;
+public abstract class BaseIntegrationTest : IClassFixture<IntegrationTestFactory>
+{
+    private readonly IServiceScope _scope;
+    protected readonly IEventRepository EventRepository;
+    protected readonly IEventUseCase EventUseCase;
+    protected readonly EventDbContext DbContext; 
+
+    protected BaseIntegrationTest(IntegrationTestFactory factory)
+    {
+        _scope = factory.Services.CreateScope();
+
+        factory.ResetDatabaseAsync().GetAwaiter().GetResult();
+
+        EventRepository = _scope.ServiceProvider.GetRequiredService<IEventRepository>();
+        EventUseCase = _scope.ServiceProvider.GetRequiredService<IEventUseCase>();
+
+        DbContext = _scope.ServiceProvider.GetRequiredService<EventDbContext>(); 
+    }
+}
---
+++ tests/Application.IntegrationTests/IntegrationTestFactory.cs
@@ -0,0 +1,73 @@
+﻿using Adapters.Secondary.Data.Context;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+using Respawn;
+using System.Data.Common;
+using Testcontainers.PostgreSql;
+
+namespace Application.IntegrationTests;
+public class IntegrationTestFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private Respawner _respawner = default!;
+    private DbConnection _connection = default!;
+
+    private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+        .WithImage("postgres:15-alpine")
+        .WithDatabase("events")
+        .WithUsername("postgres")
+        .WithPassword("postgres")
+        .Build();
+
+    public async Task ResetDatabaseAsync()
+    {
+        await _respawner.ResetAsync(_connection);
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            var descriptor = services
+            .SingleOrDefault(s => s.ServiceType == typeof(DbContextOptions<EventDbContext>));
+
+            if (descriptor is not null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<EventDbContext>(options =>
+            {
+                options.UseNpgsql(_dbContainer.GetConnectionString());
+            });
+        });
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _dbContainer.StartAsync();
+
+        using (var scope = Services.CreateScope())
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService<EventDbContext>();
+            await dbContext.Database.MigrateAsync();
+        }
+
+        _connection = new NpgsqlConnection(_dbContainer.GetConnectionString());
+        await _connection.OpenAsync();
+
+        _respawner = await Respawner.CreateAsync(_connection, new RespawnerOptions
+        {
+            DbAdapter = DbAdapter.Postgres,
+            SchemasToInclude = new[] { "public" }
+        });
+    }
+    public new async Task DisposeAsync()
+    {
+        await _connection.CloseAsync();
+        await _dbContainer.StopAsync();
+    }
+}
