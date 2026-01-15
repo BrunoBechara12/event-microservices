74f08b97f00cf2f6373c817bb6d519c9d3d8d411
Bruno Bechara <Brunosbechara@gmail.com>
2026-01-14 22:38:36 -0300

    feat(guest): add Guest repository

---
+++ Services/GuestService/src/Adapters.Secondary.Data/Repositories/GuestRepository.cs
@@ -0,0 +1,48 @@
+﻿using Adapters.Secondary.Data.Context;
+using Domain.Entities;
+using Domain.Ports;
+using Microsoft.EntityFrameworkCore;
+
+namespace Adapters.Secondary.Data.Repositories;
+public class GuestRepository : IGuestRepository
+{
+    private readonly GuestDbContext _context;
+
+    public GuestRepository(GuestDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Guest>?> Get()
+    {
+        return await _context.Guests.ToListAsync();
+    }
+
+    public async Task<Guest?> GetById(int id)
+    {
+        return await _context.Guests.FirstOrDefaultAsync(g => g.Id == id);
+    }
+
+    public async Task<Guest> Create(Guest guest)
+    {
+        var newGuest = await _context.Guests.AddAsync(guest);
+
+        await _context.SaveChangesAsync();
+
+        return newGuest.Entity;
+    }
+
+    public async Task Update(Guest guest)
+    {
+        _context.Guests.Update(guest);
+
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Delete(Guest guest)
+    {
+        _context.Guests.Remove(guest);
+
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ Services/GuestService/src/Domain/Ports/IGuestRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+
+namespace Domain.Ports;
+public interface IGuestRepository
+{
+    Task<List<Guest>?> Get();
+    Task<Guest?> GetById(int id);
+    Task<Guest> Create(Guest guest);
+    Task Update(Guest guest);
+    Task Delete(Guest guest);
+}
