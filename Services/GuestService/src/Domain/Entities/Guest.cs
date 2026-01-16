1760a45e24831cd21dafb77dea1b3fdaf1043dc0
Bruno Bechara <Brunosbechara@gmail.com>
2026-01-15 23:14:26 -0300

    fix(guest): add Guest Id

--- Services/GuestService/src/Domain/Entities/Guest.cs
+++ Services/GuestService/src/Domain/Entities/Guest.cs
@@ -11,6 +11,7 @@ public sealed class Guest
     }
 
     [Key]
+    public int Id { get; private set; }
     public string Name { get; private set; }
     public string Email { get; private set; }
     public string PhoneNumber { get; private set; }
